4b1741f670875443523e65f09c90a682ca35fe66
artursil <artur.silicki@gmail.com>
2023-01-10 19:44:07 +0100

    DictPons finished

--- pons.py
+++ pons.py
@@ -9,8 +9,8 @@ import pandas as pd
 import shutil
 
 from pathlib import Path
-from google_images_download import google_images_download
-response = google_images_download.googleimagesdownload() 
+from simple_image_download import simple_image_download as simp
+response = simp.simple_image_download
 
 
 def downloadimages(query):
@@ -23,41 +23,124 @@ def downloadimages(query):
     # aspect ratio denotes the height width ratio
     # of images to download. ("tall, square, wide, panoramic")
     query = query.split(",")[0]
-    arguments = {"keywords": query,
-                 "format": "jpg",
-                 "limit": 1,
-                 "print_urls": False,
-                 "size": "medium",}
-    
-    response.download(arguments)
-    image = Path.cwd() / "downloads" / query
-    return Path(list(image.glob("*"))[0])
+    response().download(query, 10)
+    image = Path.cwd() / "simple_images" / query
+    files = list(image.glob("*"))
+    file = [x for x in files if x.stat().st_size > 10000][0]    
+    for f in files:
+        if f != file:
+            f.unlink()
+    return file
+
+
+DER_DIE_DAS = {"masculine": "der",
+               "neuter": "das",
+               "feminine": "die",
+               "m": "der",
+               "nt": "das",
+               "f": "die",
+               "plural": "die"}
+
+
+def word_processing(word: str, sense: str):
+    return word.replace("·", "") + sense
+
+
+def de_verb_processing(verb: str, **kwargs):
+    pos = kwargs.get("pos")
+    if pos == "reflexive verb":
+        verb = f"sich {verb}"
+    return verb
+
+
+def en_verb_processing(verb: str, **kwargs):
+    if "to " in verb:
+        return verb
+    return f"to {verb}"
+
+
+verb_processors = {
+    "de": de_verb_processing,
+    "en": en_verb_processing,
+}
+
+
+def verb_processing(verb: str, lang: str, pos: str):
+    return verb_processors[lang](verb, pos=pos)
+
+
+def de_noun_processing(noun: str, **kwargs):
+    gender = kwargs.get("gender", "")
+    return f"{DER_DIE_DAS[gender]} {noun.capitalize()}"
+
+
+def en_noun_processing(noun: str, **kwargs):
+    return noun
+
+
+noun_processors = {
+    "de": de_noun_processing,
+    "en": en_noun_processing,
+}
+
+
+def noun_processing(noun: str, lang: str, gender: str):
+    return verb_processors[lang](noun, gender=gender)
+
+
+def de_pos_processing(pos: str, **kwargs):
+    word = kwargs.get("word", "")
+    if "verb" in pos:
+        if "sich" in word.split():
+            pos = "reflexive verb"
+        else:
+            pos = "transitive verb"
+    return pos
+
+
+def en_pos_processing(pos: str, **kwargs):
+    return pos
+
+
+pos_processors = {
+    "de": de_pos_processing,
+    "en": en_pos_processing,
+}
+
+
+def pos_processing(pos: str, lang: str, word: str):
+    pos = pos.lower()
+    pos_map = {"adj": "adjective", "adv": "adverb"}
+    pos = pos_map[pos] if pos in pos_map.keys() else pos
+    return pos_processors[lang](pos, word=word)
 
 
 class DictFactory(ABC):
-    def __init__(self):
-        self.original_word = ""
-        self.original_pos = ""
-        self.input_lang = ""
-        self.src_lang = ""
-        self.dst_lang = ""
-        self.collections_path = Path()
+    def __init__(self, original_word: str,
+                 pos: Optional[str],
+                 input_lang: Optional[str],
+                 src_lang: str,
+                 dst_lang: str,
+                 source: str,
+                 collections_path: Path = Path("~/.local/share/Anki2/User 1/collection.media")):
+        self.original_word = original_word
+        self.original_pos = pos
+        self.input_lang = input_lang
+        self.src_lang = src_lang
+        self.dst_lang = dst_lang
+        self.source = source
+        self.collections_path = collections_path.expanduser()
 
     @abstractmethod
     def get_entries():
         pass
 
     @property
-    @abstractmethod
-    def entries(self):
-        pass
-
-    @property
     def image(self):
         if self.input_lang == self.dst_lang:
-            img_word = self.word
-        else:
             img_word = self.definition
+        else:
+            img_word = self.word
         image_path = downloadimages(img_word)
         img_name = image_path.parent.stem + image_path.suffix
         img_dst = self.collections_path / img_name
@@ -100,18 +183,23 @@ class DictFactory(ABC):
         pass
 
     def anki_row(self):
+        if self.input_lang == self.src_lang:
+            processed_word = self.definition
+        else:
+            processed_word = self.word
+
         row_dict = {
-            "German": self.processed_word,  # TODO 
-            "Picture": self.get_image,
+            "German": processed_word,
+            "Picture": self.image,
             "English": self.definition,
             "Audio": self.audio,
             "Sample sentence": self.example_src,
             "Plural and inflected forms": self.tenses_plural,
             "German Alternatives": "",
-            "English Alternatives": self.example_translation,
+            "English Alternatives": self.example_dst,
             "Part of speech": self.pos,
             "original_word": self.word,
-            "Source": "Language Reactor"
+            "Source": self.source
         }
         return row_dict
 
@@ -140,7 +228,6 @@ class PonsEntries():
                     arabs_n = len(arabs)
                     entries_n += arabs_n
                     self.processed_entries[lang]["rom_n"].extend([ix] * arabs_n)
-                    print(len(self.processed_entries[lang]["rom_n"]))
                     self.process_headwords(lang=lang, rom=rom, n=arabs_n)
                     self.process_headword_full(lang=lang, rom=rom, n=arabs_n)
                     wc = self.process_wordclass(lang=lang, rom=rom, n=arabs_n)
@@ -151,14 +238,14 @@ class PonsEntries():
 
     def process_headwords(self, lang: str, rom: dict, n: int):
         self.processed_entries[lang]["headword"].extend([rom["headword"]] * n)
-        
-    def process_headword_full(self, lang:str, rom: dict, n: int):
+
+    def process_headword_full(self, lang: str, rom: dict, n: int):
         hf = rom["headword_full"]
         hf = BeautifulSoup(hf, features="lxml")
         if span := hf.find("span", {"class": "flexion"}):
-            flexicon = span.text
+            flexion = span.text
         else:
-            flexicon = ""
+            flexion = ""
         if span := hf.find("span", {"class": "phonetics"}):
             phonetics = span.text
         else:
@@ -171,7 +258,7 @@ class PonsEntries():
         #     pos = span.acronym.get("title")
         # else:
         #     pos = ""
-        self.processed_entries[lang]["flexicon"].extend([flexicon] * n)
+        self.processed_entries[lang]["flexion"].extend([flexion] * n)
         self.processed_entries[lang]["phonetics"].extend([phonetics] * n)
         self.processed_entries[lang]["gender_src"].extend([gender] * n)
 
@@ -181,7 +268,7 @@ class PonsEntries():
             return wordclass
         else:
             raise KeyError("wordclass")
-        
+
     def process_arab(self, lang: str, arab: dict, wc: str):
         # Sens
         sens = BeautifulSoup(arab["header"], features="lxml")
@@ -210,7 +297,6 @@ class PonsEntries():
         self.processed_entries[lang]["target"].append(target)
         self.processed_entries[lang]["gender_dst_dst"].append(gender)
         self.processed_entries[lang]["target_desc"].append(target_desc)
-
 
         # Examples
         examples_src1 = []
@@ -255,21 +341,159 @@ class PonsEntries():
         return df
 
 
+# class PonsDict(DictFactory):
+#     def __init__(self,
+#                  word: str,
+#                  pos: Optional[str] = None,
+#                  input_lang: Optional[str] = None,
+#                  manual_selection: bool = False,
+#                  src_lang: str = "en",
+#                  dst_lang: str = "de"):
+#         self.original_word = word
+#         self.original_pos = pos
+#         self.input_lang = input_lang
+#         self.manual_selection = manual_selection
+#         self.src_lang = src_lang
+#         self.dst_lang = dst_lang
+#
+#         self.api = f"https://api.pons.com/v1/dictionary?l={target_lang}{src_lang}&q={word}"
+#         self.processed_entries = PonsEntries(self.entries)
+#
+#     @property
+#     def row(self):
+#         entries = self.entries
+#         if self.original_pos is None:
+#             pass
+#
+#             
+#
+#     def get_entries(self):
+#         if r := requests.get(self.api, headers={"X-Secret": PONS_SECRET}).text:
+#             return json.loads(r)
+#         else:
+#             return r
+#
+#     @property
+#     def entries():
+#         pons = PonsEntries(self.get_entries())
+#         df = pons.get_df()
+#         return df
+#
+#     def definition(self):
+#         pass
+#
+#     def example_src(self):
+#         pass
+#
+#     def audio(self):
+#         pass
+#
+#     @property
+#     def example_dst(self):
+#         pass
+#
+#     @property
+#     def tenses_plural(self):
+#         pass
+#
+#     @property
+#     def pos(self):
+#         pass
+#
+#     @property
+#     def word(self):
+#         pass
+#
+#
+# class PonsDeEn(PonsDict):
+#     def __init__(self,
+#                  word: str,
+#                  input_lang: Optional[str] = None,
+#                  pos: Optional[str] = None):
+#         super().__init__(word, pos, input_lang, "en", "de")
+#
+#     def definition(self):
+#         definition = word_processing(self.row.target)
+#         lang = self.src_lang if self.src_lang != self.input_lang else \
+#             self.dst_lang
+#         if "verb" in self.pos:
+#             word = verb_processing(verb=word,
+#                                    lang=lang,
+#                                    pos=self.pos,
+#                                    )
+#         if self.pos == "noun":
+#             word = noun_processing(noun=word,
+#                                    lang=lang,
+#                                    gender=self.row.gender_src,
+#                                    )
+#
+#     def example_src(self):
+#         return self.row.examples_src
+#
+#     def audio(self):
+#         pass
+#
+#     @property
+#     def example_dst(self):
+#         return self.row.examples_dst
+#
+#     @property
+#     def tenses_plural(self):
+#         return self.row.flexion
+#
+#     @property
+#     def pos(self):
+#         if self.pos is None:
+#             self.pos = self.row.pos
+#
+#     @property
+#     def input_lang(self):
+#         if self.input_lang is None:
+#             self.input_lang = self.row.lang
+#
+#     @property
+#     def word(self):
+#         word = word_processing(self.original_word, sense=self.row.sense)
+#         if "verb" in self.pos:
+#             word = verb_processing(verb=word,
+#                                    lang=self.input_lang,
+#                                    pos=self.pos,
+#                                    )
+#         if self.pos == "noun":
+#             word = noun_processing(noun=word,
+#                                    lang=self.input_lang,
+#                                    gender=self.row.gender_src,
+#                                    )
+#         return word
+
 class PonsDict(DictFactory):
     def __init__(self,
                  word: str,
                  pos: Optional[str] = None,
                  input_lang: Optional[str] = None,
+                 manual_selection: bool = False,
                  src_lang: str = "en",
-                 target_lang: str = "de"):
-        self.original_word = word
-        self.original_pos = pos
-        self.input_lang = input_lang
-        self.src_lang = src_lang
-        self.dst_lang = dst_lang
+                 dst_lang: str = "de",
+                 source: str = "manual",
+                 # collections_path: Path,
+                 ):
+        super().__init__(word, pos, input_lang, src_lang, dst_lang, source)
+        self.manual_selection = manual_selection
 
-        self.api = f"https://api.pons.com/v1/dictionary?l={target_lang}{src_lang}&q={word}"
-        self.processed_entries = PonsEntries(self.entries)
+        self.api = f"https://api.pons.com/v1/dictionary?l={dst_lang}{src_lang}&q={word}"
+        self.entries = self.process_entries()
+        self.input_lang = self.get_input_lang(self.input_lang)
+        # self.processed_entries = PonsEntries(self.entries)
+
+    @property
+    def row(self) -> pd.Series:
+        df = self.entries
+        if self.original_pos is not None:
+            pos = pos_processing(pos=self.original_pos, lang=self.dst_lang,
+                                 word=self.original_word)
+            if row := df.loc[df.pos.isin(pos)]:
+                return row.iloc[0]
+        return df.iloc[0]
 
     def get_entries(self):
         if r := requests.get(self.api, headers={"X-Secret": PONS_SECRET}).text:
@@ -277,67 +501,35 @@ class PonsDict(DictFactory):
         else:
             return r
 
-    def select_row(self):
-        pass
+    def process_entries(self):
+        pons = PonsEntries(self.get_entries())
+        df = pons.get_df()
+        return df
 
     @property
-    def entries(self):
-        return self.get_entries()
-
-    def definition(self):
-        pass
-
-    def example_src(self):
-        pass
-
     def audio(self):
-        pass
+        ""
 
     @property
-    def example_dst(self):
-        pass
-
-    @property
-    def tenses_plural(self):
-        pass
-
-    @property
-    def pos(self):
-        pass
-
-    @property
-    def word(self):
-        pass
-
-
-class PonsDeEn(PonsDict):
-    def __init__(self,
-                 word: str,
-                 input_lang: Optional[str] = None,
-                 pos: Optional[str] = None):
-        super().__init__(word, pos, input_lang, "en", "de")
-        self.select_definition()
-
     def definition(self):
-        definition = word_processing(self.row.target)
+        definition = word_processing(self.row.target, "")
         lang = self.src_lang if self.src_lang != self.input_lang else \
             self.dst_lang
         if "verb" in self.pos:
-            word = verb_processing(verb=word,
-                                   lang=lang,
-                                   pos=self.pos,
-                                   )
+            definition = verb_processing(verb=definition,
+                                         lang=lang,
+                                         pos=self.pos,
+                                         )
         if self.pos == "noun":
-            word = noun_processing(noun=word,
-                                   lang=lang,
-                                   gender=self.row.gender_src,
-                                   )
+            definition = noun_processing(noun=definition,
+                                         lang=lang,
+                                         gender=self.row.gender_src,
+                                         )
+        return definition
 
+    @property
     def example_src(self):
         return self.row.examples_src
-
-    def audio(self):
-        pass
 
     @property
     def example_dst(self):
@@ -349,13 +541,13 @@ class PonsDeEn(PonsDict):
 
     @property
     def pos(self):
-        if self.pos is None:
-            self.pos = self.row.pos
+        if self.original_pos is None:
+            return self.row.pos
 
-    @property
-    def input_lang(self):
-        if self.input_lang is None:
-            self.input_lang = self.row.lang
+    def get_input_lang(self, input_lang):
+        if input_lang is None:
+            return self.row.lang
+        return input_lang
 
     @property
     def word(self):
@@ -372,64 +564,15 @@ class PonsDeEn(PonsDict):
                                    )
         return word
 
-DER_DIE_DAS = {"masculine": "der",
-               "neuter": "das",
-               "feminine": "die",
-               "m": "der",
-               "nt": "das",
-               "f": "die",
-               "plural": "die"}
-
-
-def word_processing(word: str, sense: str):
-    return word.replace("·", "") + sense
-
-
-def de_verb_processing(verb: str, **kwargs):
-    pos = kwargs.get("pos")
-    if pos == "reflexive verb":
-        verb = f"sich {verb}"
-    return verb
-
-
-def en_verb_processing(verb: str, **kwargs):
-    return f"to {verb}"
-
-
-verb_processors = {
-    "de": de_verb_processing,
-    "en": en_verb_processing,
-}
-
-
-def verb_processing(verb: str, lang: str, pos: str):
-    return verb_processors[lang](verb, pos=pos)
-
-
-def de_noun_processing(noun: str, **kwargs):
-    gender = kwargs.get("gender", "")
-    return f"{DER_DIE_DAS[gender]} {noun.capitalize()}"
-
-
-def en_noun_processing(noun: str, **kwargs):
-    return noun
-
-
-noun_processors = {
-    "de": de_noun_processing,
-    "en": en_noun_processing,
-}
-
-
-def noun_processing(noun: str, lang: str, gender: str):
-    return verb_processors[lang](noun, gender=gender)
-
 
 if __name__ == "__main__":
     pons = PonsDict("laufen")
     # print(pons.entries)
     entries = pons.entries
-    pons = PonsEntries(entries)
-    df = pons.get_df()
-    df.to_csv("test_pons.csv")
+    # entries = PonsEntries(entries)
+    anki_row = pons.anki_row()
+    # df = pons.get_df()
+    # df.to_csv("test_pons.csv")
+    print(anki_row)
     __import__('pdb').set_trace()
+
